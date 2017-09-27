6d010306598837361d96c7d64718d1b7b379eb83
DDRAGON <davide.0829@gmail.com>
2017-09-27 20:43:53 +0900

    POSTのデータを chunk で受け取るように変更

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ const server = http.createServer((req, res) => {
 			rs.pipe(res);
 			break;
 		case 'POST':
+			let body = [];
 			req.on('data', (data) => {
-				const decoded = decodeURIComponent(data);
+				body.push(chunk);
+			}).on('end', () => {
+				body = Buffer.concat(body).toString();
+				console.info('[' + now + '] Data posted: ' + body);
+				const decoded = decodeURIComponent(body);
 				console.info('[' + now + '] 投稿: ' + decoded);
-				res.write('<!DOCTYPE html><html lang="jp"><head><meta charset="utf-8"></head><body><h1>' +
-					decoded + 'が投稿されました</h1></body></html>');
+				res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+					  decoded + 'が投稿されました</h1></body></html>');
 				res.end();
 			});
 			break;
