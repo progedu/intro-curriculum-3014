9580657c041cd6bbea9b30538b9624d4e8cb29f8
bobchin <kinjo.bobchin@gmail.com>
2018-07-29 23:32:27 +0900

    修正完了!

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req, res) => {
-	const now = new Date();
-	console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
-	res.writeHead(200, {
-		'Content-Type': 'text/html; charset=utf-8'
-	});
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
 
-	switch (req.method) {
-		case 'GET':
-			const fs = require('fs');
-			const rs = fs.createReadStream('./form.html');
-			rs.pipe(res);
-			break;
-		case 'POST':
-			let body = [];
-			req.on('data', (chunk) => {
-				body.push(chunk);
-			}).on('end', () => {
-				body = Buffer.concat(body).toString();
-				const decoded = decodeURIComponent(body);
-				console.info('[' + now + '] 投稿: ' + decoded);
-				res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-					  decoded + 'が投稿されました</h1></body></html>');
-				res.end();
-			});
-			break;
-		default:
-			break;
-	}
-
-}).on('error', (e) => {
-	console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-	console.error('[' + new Date() + '] Client Error', e);
-});
+    switch (req.method) {
+      case 'GET':
+        const fs = require('fs');
+        const rs = fs.createReadStream('./form.html');
+        rs.pipe(res);
+        break;
+      case 'POST':
+        let body = [];
+        req
+          .on('data', chunk => {
+            body.push(chunk);
+          })
+          .on('end', () => {
+            body = Buffer.concat(body).toString();
+            const decoded = decodeURIComponent(body);
+            console.info('[' + now + '] 投稿: ' + decoded);
+            res.write(
+              '<!DOCTYPE html><html lang="ja"><body><h1>' +
+                decoded +
+                'が投稿されました</h1></body></html>'
+            );
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', e => {
+    console.error('[' + new Date() + '] Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('[' + new Date() + '] Client Error', e);
+  });
 const port = 8000;
 server.listen(port, () => {
-	console.info('[' + new Date() + '] Listening on ' + port);
+  const createdDate = new Date();
+  console.info(
+    `[ ${createdDate} ] Listening on ${port} please open http://localhost:8000/`
+  );
 });
