4c012b38c2dad2dc58d405a96b213d59345f7645
Boo800 <school.act5008@gmail.com>
2019-02-14 01:54:34 +0000

    入門コースの3章14節の練習

--- index.js
+++ index.js
@@ -20,9 +20,20 @@ const server = http.createServer((req, res) => {
 			}).on('end', () => {
 				body = Buffer.concat(body).toString();
 				const decoded = decodeURIComponent(body);
+				// decodedから文字列をぬきだす
+				// [^\x01-\x7E] 全角の文字の検索 /^[^\x01-\x7E\xA1-\xDF]+$/
+				// yaki-shabu=しゃぶしゃぶ&name=太郎が投稿されました
+				// 'yaki-shabu=' + 全角文字が1文字以上 + '&name=' + 0文字以上の何でもよい文字列
+				// match(/yaki-shabu=([^\x01-\x7E\xA1-\xDF]+)&name=(.*)/i);
+				const stringArray = decoded.match(/yaki-shabu=([^\x01-\x7E\xA1-\xDF]+)&name=(.*)/i);
+				
 				console.info('[' + now + '] 投稿: ' + decoded);
+
 				res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-					  decoded + 'が投稿されました</h1></body></html>');
+					  stringArray[2] + 'は、' + stringArray[1] +'が食べたいです。</h1></body></html>');
+
+				// res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+				// 	  decoded + 'が投稿されました</h1></body></html>');
 				res.end();
 			});
 			break;
