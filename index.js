6e834238d62f144ebabd3de6272a310d0de1b0cf
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2020-01-07 05:21:22 +0900

    CSSを読み込ませる&名前に&と=を追加しても送信できるようにする

--- index.js
+++ index.js
@@ -10,8 +10,27 @@ const server = http.createServer((req, res) => {
 	switch (req.method) {
 		case 'GET':
 			const fs = require('fs');
-			const rs = fs.createReadStream('./form.html');
-			rs.pipe(res);
+			let path = '';
+			
+			//複数のファイルを読み込ませる!
+			switch (req.url)
+			{
+				case '/':
+					res.writeHead(200, {"Content-Type": "text/html"})
+					{
+						path = './form.html';
+						const rs = fs.createReadStream(path,{encoding: "UTF-8"}).pipe(res);
+					}
+					break;
+				case '/style.css':
+					//wrteHeadじゃないと読み込まない&拡張子を宣言してあげる必要がある
+					res.writeHead(200, {"Content-Type": "text/css"})
+					{
+						path = './style.css';
+						const css = fs.createReadStream(path,{encoding: "UTF-8"}).pipe(res);
+					}
+					break;
+			}
 			break;
 		case 'POST':
 			let body = [];
@@ -21,6 +40,43 @@ const server = http.createServer((req, res) => {
 				body = Buffer.concat(body).toString();
 				const decoded = decodeURIComponent(body);
 				console.info('[' + now + '] 投稿: ' + decoded);
+				//名前の&と=対策
+				var result = decoded.split('=');
+				if(result.length > 2)
+				{
+					for(var i = 2; i < result.length; i++)
+					{
+						result[1] += "=" + result[i];
+					}
+					//result[2] = result[result.length];
+					result.length = 2;
+				}
+
+				result = result[1].split('&');
+				
+				if(result.length > 2)
+				{
+					for(var i = 0; i < result.length-1; i++)
+					{
+						result[0] += "&" + result[i];     
+					}
+					result[1] = result[result.length-1];
+					result.length = 2;
+				}
+				console.info('[' + now + '] result分割: ' + result);
+				var name = result[0];
+
+				result = result[1].split('=');
+
+				if(result[1] === "焼き肉")
+				{
+					console.info('[' + name + '] 焼肉: ' + result);
+				}
+				else
+				{
+					console.info('[' + name + '] しゃぶしゃぶ: ' + result);
+				}
+
 				res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
 					  decoded + 'が投稿されました</h1></body></html>');
 				res.end();
