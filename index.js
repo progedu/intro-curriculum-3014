c16cb6080f29c4ccd9fcce80c308e89c605ed1d8
shakyStone <shakystone@icloud.com>
2020-04-30 12:53:54 +0900

    console.info 変更

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const server = http.createServer((req, res) => {
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
         const answer = qs.parse(decoded);
-        console.info('[' + now + '] 投稿: ' + answer['yaki-shabu']);
+        console.info('[' + now + '] 投稿: ' + answer['name'] + ' ' + answer['yaki-shabu']);
         res.write(`<!DOCTYPE html><html lang=\\"ja\\"><body><h1>${answer['name']}さんは${answer['yaki-shabu']}に投票しました</h1></body></html>`);
         res.end();
       });
