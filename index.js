65f60673e566cfe9ea8776624b39443273116545
merinter19 <merinter19@gmail.com>
2020-05-01 20:20:50 +0900

    名前を入力できるように変更

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ const server = http.createServer((req, res) => {
       rs.pipe(res);
       break;
     case 'POST':
+      const qs = require('querystring');
       let rawData = '';
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        const answer = qs.parse(decoded);
+        console.info('[' + now + '] 投稿: ' + answer['name'] +
+        'さんは' + answer['yaki-shabu'] + 'に投票しました');
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+          answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投票しました</h1></body></html>');
         res.end();
       });
       break;
