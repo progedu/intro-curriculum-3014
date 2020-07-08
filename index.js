78f5fbbc2b61e60e837e4ec20397c365d2f3614a
sugar-rookie <0.luckystrike@gmail.com>
2020-07-08 19:23:31 +0900

    名前入力を追加し、投票結果を名前付きで出力するよう修正

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+		console.info('[' + now + '] 投稿: ' + decoded);
+		const qs = require('querystring');
+		const answer = qs.parse(decoded);
+		const name = answer['name'];
+		const detected = answer['yaki-shabu'];
+
+        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + name + 'さんは' +
+          detected + 'に投票しました</h1></body></html>');
         res.end();
       });
       break;
