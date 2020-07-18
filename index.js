0d45d83c2342d59c520005ae50f6a9c975f1a02b
takiyama <shouta_19n1200083@nnn.ed.jp>
2020-07-18 19:08:35 +0900

    投稿メッセージを改良

--- index.js
+++ index.js
@@ -7,33 +7,44 @@ const server = http.createServer((req, res) => {
     'Content-Type': 'text/html; charset=utf-8'
   });
 
+  //リクエスト処理
   switch (req.method) {
-    case 'GET':
+    //GETの処理
+    case 'GET': 
       const fs = require('fs');
       const rs = fs.createReadStream('./form.html');
+      //resにファイルをpipe
       rs.pipe(res);
       break;
+    //POSTの処理
     case 'POST':
       let rawData = '';
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        const qs = require('querystring');
+        const answer = qs.parse(decoded);
+        console.info('[' + now + '] 投稿者:' + answer['name'] + ' 回答:' + answer['yaki-shabu']);
+        //投稿への返答
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+          answer['name'] + "さんは" + answer['yaki-shabu'] + 'に投票しました</h1></body></html>');
         res.end();
       });
       break;
     default:
       break;
   }
+  //エラー処理
 }).on('error', (e) => {
+  //サーバー側のエラー
   console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
+  //クライアント側のエラー
   console.error('[' + new Date() + '] Client Error', e);
 });
 const port = 8000;
+//8000ポートをlisten
 server.listen(port, () => {
   console.info('[' + new Date() + '] Listening on ' + port);
 });
