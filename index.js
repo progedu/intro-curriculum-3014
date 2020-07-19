c87ea81b7090e1df4cce1fceea1c5b502a5bb385
Fukuchi Kentaro <ken3ken@me.com>
2020-07-19 16:52:23 +0900

    名前も投稿できるように変更

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ const server = http.createServer((req, res) => {
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
         console.info('[' + now + '] 投稿: ' + decoded);
+        const qs = require('querystring');
+        const answer = qs.parse(decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+          answer['name'] + 'さんは' + answer['yaki-shabu'] +
+          'に投票しました</h1></body></html>');
         res.end();
       });
       break;
