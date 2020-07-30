53aab85f916fb410d0deb572ecc721b72772f9e8
Waka-Ruby-01 <64844190+Waka-Ruby-01@users.noreply.github.com>
2020-07-30 14:46:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ const server = http.createServer((req, res) => {
 			req.on('data', (chunk) => {
 				body.push(chunk);
 			}).on('end', () => {
-				body = Buffer.concat(body).toString();
-				const decoded = decodeURIComponent(body);
-				console.info('[' + now + '] 投稿: ' + decoded);
+				const qs = require('querystring');
+        			const answer = qs.parse(rawData);
+        			const body = answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投票しました';
+        			console.info('[' + now + '] ' + body);
 				res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-					  decoded + 'が投稿されました</h1></body></html>');
+					  body + '</h1></body></html>');
 				res.end();
 			});
 			break;
