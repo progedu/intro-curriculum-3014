4b20ba9acfdeb94993e21698eff8f7a904273d6f
blue-one1975 <hiroshitakeda428@yahoo.co.jp>
2020-08-04 12:24:56 +0900

    名前記入欄等を追加し、投稿結果の表示を変更

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  console.info(`[ ${now} ] Request by ${req.connection.remoteAddress}`);
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
-
   switch (req.method) {
     case 'GET':
       const fs = require('fs');
@@ -14,26 +13,36 @@ const server = http.createServer((req, res) => {
       rs.pipe(res);
       break;
     case 'POST':
-      let rawData = '';
+      let rawDate = '';
       req.on('data', (chunk) => {
-        rawData = rawData + chunk;
+        rawDate += chunk;
       }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+        const qs = require('querystring');
+				const decoded = decodeURIComponent(rawDate);
+				console.info(`[ ${now} ] POSTED: ${decoded}`);
+				const answer = qs.parse(decoded);
+        res.write(`<!DOCTYPE html>
+				<html lang="ja">
+				<head>
+					<meta charset="UTF-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				</head>
+				<body>
+					<h1>${answer['name']}さんは${answer['which']}に投稿しました</h1>
+				</body>
+				</html>`);
         res.end();
       });
       break;
     default:
       break;
   }
-}).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
-});
+  }).on('error', e => {
+      console.error(`[ ${new Date()} ] Server Error`, e);
+  }).on('clienterror', e => {
+      console.error(`[ ${new Date()} ] Client Error`, e);
+  });
 const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+    console.info(`[ ${new Date()} ] Listening On ${port}`);
 });
