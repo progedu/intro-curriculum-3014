844e6e273e63ecc6bd46dd3d8698715e9c2acc81
shinfujita <shin4522@gmail.com>
2020-10-22 15:49:06 +0900

    3章15節の課題

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const server = http.createServer((req, res) => {
     case 'GET':
       const fs = require('fs');
       const rs = fs.createReadStream('./form.html');
+
       rs.pipe(res);
       break;
     case 'POST':
@@ -18,10 +19,11 @@ const server = http.createServer((req, res) => {
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+        const qs = require('querystring');
+        const answer = qs.parse(rawData);
+        const body = answer['name'] + 'さんが' + answer['yaki-shabu'] + 'に投票しました';
+        console.info(body);
+        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + body + '</h1></body></html>');
         res.end();
       });
       break;
