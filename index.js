72a18ca4923f29c26ccb84c24e46e48babee6f9b
Norihisa Kuwajima <58217615+KJiman@users.noreply.github.com>
2020-11-07 00:27:01 +0900

    フォームに氏名入力欄の追加、送信完了後の画面遷移時に氏名を表示

--- index.js
+++ index.js
@@ -13,15 +13,28 @@ const server = http.createServer((req, res) => {
       const rs = fs.createReadStream('./form.html');
       rs.pipe(res);
       break;
-    case 'POST':
+		case 'POST':
+			const qs = require('querystring');
       let rawData = '';
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
+				const decoded = decodeURIComponent(rawData);
+				const answer = qs.parse(decoded);
         console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+        res.write(`
+					<!DOCTYPE html>
+						<html lang="ja">
+						<head>
+							<meta charset="UTF-8" />
+							<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+							<title>投稿完了</title>
+						</head>
+						<body>
+							<h1>${answer['name']}さんは${answer['yaki-shabu']}に投票しました。</h1>
+						</body>
+					</html>
+				`);
         res.end();
       });
       break;
