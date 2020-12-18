747c8eeb2169c7eed979574eb2a6a41d3bb7e060
maki-code <63586566+maki-code@users.noreply.github.com>
2020-12-18 16:50:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req, res) => {
-	const now = new Date();
-	console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
-	res.writeHead(200, {
-		'Content-Type': 'text/html; charset=utf-8'
-	});
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
 
-	switch (req.method) {
-		case 'GET':
-			const fs = require('fs');
-			const rs = fs.createReadStream('./form.html');
-			rs.pipe(res);
-			break;
-		case 'POST':
-			let body = [];
-			req.on('data', (chunk) => {
-				body.push(chunk);
-			}).on('end', () => {
-				body = Buffer.concat(body).toString();
-				const decoded = decodeURIComponent(body);
-				console.info('[' + now + '] 投稿: ' + decoded);
-				res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-					  decoded + 'が投稿されました</h1></body></html>');
-				res.end();
-			});
-			break;
-		default:
-			break;
-	}
-
-}).on('error', (e) => {
-	console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-	console.error('[' + new Date() + '] Client Error', e);
-});
+    switch (req.method) {
+      case 'GET':
+        const fs = require('fs');
+        const rs = fs.createReadStream('./form.html');
+        rs.pipe(res);
+        break;
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData = rawData + chunk;
+          })
+          .on('end', () => {
+            const qs = require('querystring');
+        const answer = qs.parse(rawData);
+        const body = answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投票しました';
+        console.info('[' + now + '] ' + body);
+            res.write(
+              '<!DOCTYPE html><html lang="ja"><body><h1>' +
+			  body + '</h1></body></html>');
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', e => {
+    console.error('[' + new Date() + '] Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('[' + new Date() + '] Client Error', e);
+  });
 const port = 8000;
 server.listen(port, () => {
-	console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('[' + new Date() + '] Listening on ' + port);
 });
