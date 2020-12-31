8fd7c2bcfab5ad67e32d0486dfdf4b645938db50
beginnerfuji <teenagefunclub10000@gmail.com>
2021-01-01 07:39:22 +0900

    フォームに名前追加、console.infoでログにも情報を記載

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ const server = http.createServer((req, res) => {
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        const qs = require('querystring');
+        const answer = qs.parse(rawData)
+        const content = answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に一票入れました。'
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+          content + '</h1></body></html>');
+        console.info('[' + now + '] 投稿: ' + answer['name'] + answer['yaki-shabu']);
         res.end();
       });
       break;
