79f5741d148c13e02fa3e86df9660860463f2ff0
yutorse <horse.sakai@blue.ocn.ne.jp>
2021-02-28 22:31:39 +0900

    名前の入力機能の実装

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ const server = http.createServer((req, res) => {
       break;
     case 'POST':
       let rawData = '';
+      const qs = require('querystring');
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
+        const answer = qs.parse(decoded);
         console.info('[' + now + '] 投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+          answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投稿しました。</h1></body></html>');
         res.end();
       });
       break;
