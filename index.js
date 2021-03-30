8438dcb12416352d345bd01a9bcb547d9f84bc08
mokuden <mokuden0083@gmail.com>
2021-03-30 22:27:32 +0900

    名前入力欄の追加

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+        const qs = require('querystring');
+        const answer = qs.parse(decoded);
+        const result = `${answer['name']}は${answer['yaki-shabu']}に投票しました`;
+        console.info('[' + now + '] 投稿:' + result);
+        res.write('<!DOCTYPE html><html lang="ja"><body><h1>'+ result +'</h1></body></html>');
         res.end();
       });
       break;
