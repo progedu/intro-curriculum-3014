80633c6796e567463547bba8e7111a9bfd6c73ca
akanekwom172 <akanekwon_dev@icloud.com>
2021-04-08 06:30:14 +0900

    アンケートに名前の入力フォームを追加

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  console.info(`[${now}] Requested by ${req.connection.remoteAddress}`);
   res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8',
   });
 
   switch (req.method) {
@@ -15,25 +15,28 @@ const server = http.createServer((req, res) => {
       break;
     case 'POST':
       let rawData = '';
-      req.on('data', (chunk) => {
+      req.on('data', chunk => {
         rawData = rawData + chunk;
       }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+        const qs = require('querystring');
+        const answer = qs.parse(rawData);
+        const body = `${answer['name']}さんは${answer['yaki-shabu']}に投票しました`;
+        console.info(`[${now}] 投稿: ${body}`);
+        res.write(
+          `<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`
+        );
         res.end();
       });
       break;
     default:
       break;
   }
-}).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+}).on('error', e => {
+  console.error(`[${new Date()}] Server Error`, e);
+}).on('clientError', e => {
+  console.error(`[${new Date()}] Client Error`, e);
 });
 const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info(`[${new Date()}] Listening on ${port}`);
 });
