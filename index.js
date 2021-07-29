c7b27128971de13c7534bcfbf73481686e29f533
Aya <ayaka19n@gmai.com>
2021-07-29 17:41:30 +0900

    名前入力を追加、メッセージ変更

--- index.js
+++ index.js
@@ -19,9 +19,18 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
+        const qs = require('querystring');
+        const answer = qs.parse(decoded);
+        console.log(answer['name']);
+        console.log(answer['yaki-shabu']);
         console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+        res.write(`
+        <!DOCTYPE html>
+        <html lang="ja">
+        <body>
+          <h1>${answer['name']} さんは ${answer['yaki-shabu']} に投票しました</h1>
+        </body>
+        </html>`);
         res.end();
       });
       break;
